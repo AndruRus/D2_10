9482d001ac364066b126a8a50317d9ac4f56228e
AndruRus <68370029+AndruRus@users.noreply.github.com>
2021-01-11 14:40:17 +0500

    Update server.py

--- server.py
+++ server.py
@@ -24,11 +24,11 @@ def fail():
     raise RuntimeError("There is an error!")
 
 if os.environ.get("APP_LOCATION") == "heroku":
-    app.run(
+    run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000)),
         server="gunicorn",
         workers=3,
     )
 else:
-    app.run(host="localhost", port=8080, debug=True)
+    run(host="localhost", port=8080, debug=True)
