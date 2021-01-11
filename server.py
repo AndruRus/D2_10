b9233305989827c1ede5b250fe1ecbc81aa56342
AndruRus <68370029+AndruRus@users.noreply.github.com>
2021-01-11 13:50:04 +0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,34 @@
+import os
+import sentry_sdk
+
+from bottle import route, run, HTTPError, HTTPResponse
+from sentry_sdk.integrations.bottle import BottleIntegration
+
+sentry_sdk.init(
+    dsn="https://986303b801c94128a4d060353238b45a@o496969.ingest.sentry.io/5572405",
+    integrations=[BottleIntegration()]
+)
+
+@route("/")
+def main():
+    result = HTTPResponse(status=200, body="OK")
+    return result
+
+@route("/success")
+def success():
+    result = HTTPResponse(status=200, body="OK")
+    return result
+
+@route("/fail")
+def fail():
+    raise RuntimeError("There is an error!")
+
+if os.environ.get("APP_LOCATION") == "heroku":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        server="gunicorn",
+        workers=3,
+    )
+else:
+    app.run(host="localhost", port=8080, debug=True)
